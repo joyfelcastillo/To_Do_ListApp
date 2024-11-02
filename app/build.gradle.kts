7eb7f767287460f30241dcd49d75cfa1611fd0c6
joyfel <joyfelcastilloc@gmail.com>
2024-11-02 11:16:32 -0300

    primer envio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
 
 
     implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
