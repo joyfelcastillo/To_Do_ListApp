5698df9de14ac8afb5ce28cfbc58aa7e7f81ad4a
sakaneno <smpaulo878@gmail.com>
2024-11-16 14:07:27 -0300

    ultimoCommit

--- app/src/main/java/com/jcastillo/listadeproyecto/MainActivity.kt
+++ app/src/main/java/com/jcastillo/listadeproyecto/MainActivity.kt
@@ -8,17 +8,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jcastillo.listadeproyecto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+*/
+        supportFragmentManager.beginTransaction().replace(R.id.principalContainer, Pantalla1Fragment()).commit()
+
         }
+
+
+
+        /*
+        supportFragmentManager.beginTransaction().replace(R.id.principalContainer, Pantalla1Fragment()).commit()
 val themeToggleButton: Button = findViewById(R.id.themeToggleButton)
         themeToggleButton.setOnClickListener{
             val nightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
@@ -29,5 +40,10 @@ val themeToggleButton: Button = findViewById(R.id.themeToggleButton)
             }
         }
 
+
+
         }
+         */
+
+
     }
--- app/src/main/java/com/jcastillo/listadeproyecto/Pantalla2Fragment.kt
+++ app/src/main/java/com/jcastillo/listadeproyecto/Pantalla2Fragment.kt
@@ -19,17 +19,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Pantalla2Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,33 +26,6 @@ class Pantalla2Fragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_pantalla2, container,false)
-        val pantalla= view.findViewById<Button>(R.id.addButton)
-        pantalla.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_pantalla2Fragment_to_pantalla1Fragment)
-
-        }
-
-
         return view
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Pantalla2Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Pantalla2Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
